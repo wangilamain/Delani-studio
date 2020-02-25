6c491197761920b7be6a283f6966497656907543
wangilamain <wangilayng@gmail.com>
2020-02-25 09:12:52 -0500

    added an alert

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ $(document).ready(function(){
     $(".management-practice").toggle();
   });
 })
+function myFunction(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    if (name == "" || email == "" || message == ""){
+      alert ("enter your name, email and a commment")
+    }
+     else alert( name + " ,thank you for your message");
+  }
